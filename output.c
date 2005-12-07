184e2ac2929429c53526dae5dd9efeda9b172f1f
graue <graue@oceanbase.org>
2005-12-07 13:18:07 -0600

    Replaced eraseline() with bsflite's to fix line erasing
    darcs-hash:20051207191807-c8bf2-c2f86f55e2d608e162a34b541b6d9450685b7789

--- output.c
+++ output.c
@@ -1,9 +1,49 @@
 #include "bsfirc.h"
 
+#include <stdio.h>
+#include <sys/time.h>
 #ifdef PLAN9
-#include  <time.h>
+#include <time.h>
+#endif
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef __MINGW32__
+#include <windows.h>
+#else
+#include <termios.h>
+#include <sys/ioctl.h>
 #endif
 
+#ifdef __DJGPP__
+#include <conio.h>
+#endif
+
+#ifdef __MINGW32__
+HANDLE          hOut;
+#endif
+
+#if !defined(PLAN9)
+#define TERMINAL_HIJINX
+#endif
+
+#ifdef TERMINAL_HIJINX
+
+#ifdef __MINGW32__
+#define TERMINAL_WINDOWS
+#else
+
+#ifdef __DJGPP__
+#define TERMINAL_CONIO
+#else
+#define TERMINAL_VT100
+#endif
+
+#endif
+
+#endif
+
+extern int screen_cols;
 extern int prompt_len;
 extern char inputbuf[513];
 extern struct Waiting *waiting;
@@ -40,10 +80,83 @@ addts_short(void)
 void
 eraseline(void)
 {
-	int x, l = strlen(inputbuf) + prompt_len;
+	int             linelen = strlen(inputbuf) + prompt_len;
+#ifdef TERMINAL_HIJINX
+	int             numcols, numrows, x;
+#ifdef TERMINAL_CONIO
+	int             desiredx, desiredy;
+#endif
 
-	for(x = 0; x < l; x++)
+#ifdef TERMINAL_WINDOWS
+	COORD           nPos;
+	CONSOLE_SCREEN_BUFFER_INFO wInfo;
+	get_screen_size();
+#endif
+
+	numrows = linelen / screen_cols;
+	numcols = linelen % screen_cols;
+
+#ifdef TERMINAL_VT100
+	if (numrows > 0)
+		printf("\033[%dA", numrows);
+
+	if (numcols > 0)
+		printf("\033[%dD", numcols);
+#endif
+#ifdef TERMINAL_WINDOWS
+	GetConsoleScreenBufferInfo(hOut, &wInfo);
+
+	nPos.X = wInfo.dwCursorPosition.X - numcols;
+	nPos.Y = wInfo.dwCursorPosition.Y - numrows;
+
+	SetConsoleCursorPosition(hOut, nPos);
+#endif
+
+#ifdef TERMINAL_CONIO
+	desiredx = wherex() - numcols;
+	desiredy = wherey() - numrows;
+
+	gotoxy(desiredx, desiredy);
+#endif
+
+	for (x = 0; x < linelen; x++)
+		putchar(' ');
+
+#ifdef TERMINAL_VT100
+	if (numrows > 0)
+		printf("\033[%dA", numrows);
+
+	if (numcols > 0)
+		printf("\033[%dD", numcols);
+#endif
+#ifdef TERMINAL_WINDOWS
+	GetConsoleScreenBufferInfo(hOut, &wInfo);
+
+	nPos.X = wInfo.dwCursorPosition.X - numcols;
+	nPos.Y = wInfo.dwCursorPosition.Y - numrows;
+
+	SetConsoleCursorPosition(hOut, nPos);
+#endif
+#ifdef TERMINAL_CONIO
+	fflush(stdout);
+
+	gotoxy(desiredx, desiredy);
+#endif
+
+#else
+	int             x;
+
+#ifdef PLAN9
+	get_screen_size();
+#endif
+
+	for (x = 0; x < linelen; x++)
 		printf("\b \b");
+#endif
+
+#ifdef COLOR
+	set_color(0);
+#endif
 }
 
 /* PROTO */
