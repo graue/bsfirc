f64ac3d7982597593419b8ef65181c8a77774d46
graue <graue@oceanbase.org>
2006-01-21 18:29:47 -0600

    Fix alloc.h prototypes.
    darcs-hash:20060122002947-c8bf2-18af42fd7c7529278374a40b2b95b2e69c5c1a60

--- alloc.h
+++ alloc.h
@@ -9,9 +9,9 @@
 
 #ifdef SMARTALLOC
 
-extern void *sm_xmalloc(size_t size);
-extern void *sm_xrealloc(void *ptr, size_t size);
-extern char *sm_xstrdup(const char *s);
+extern void *sm_xmalloc(char *fname, int lineno, size_t size);
+extern void *sm_xrealloc(char *fname, int lineno, void *ptr, size_t size);
+extern char *sm_xstrdup(char *fname, int lineno, const char *s);
 #define xmalloc(x)      sm_xmalloc( __FILE__, __LINE__, (x))
 #define xrealloc(p,x)   sm_xrealloc(__FILE__, __LINE__, (p), (x))
 #define xstrdup(s)      sm_xstrdup( __FILE__, __LINE__, (s))
